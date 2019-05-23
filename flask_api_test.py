ce90fc0fdc3f60f106fc1f27807c0676b6b62371
林祥平 <s.0v0xx@outlook.jp>
2019-05-23 15:18:54 +0900

    changed

--- flask_api_test.py
+++ flask_api_test.py
@@ -7,13 +7,13 @@ Created on Wed May 22 17:40:50 2019
 """
 
 # 必要なモジュールの読み込み
-
+from bottle import route, run
 from flask import Flask, jsonify, make_response, request
-#import numpy as np
-#import os
-#from PIL import Image
-#import models
-#import io
+import numpy as np
+import os
+from PIL import Image
+import models
+import io
 
 HEIGHT = 144
 WIDTH = 256
@@ -40,49 +40,49 @@ all_list = fish_list + ebikani_list + ika_list + kai_list
 
 # Flaskクラスのインスタンスを作成
 # __name__は現在のファイルのモジュール名
-api = Flask(__name__)
+#api = Flask(__name__)
 
 
 
 # GETの実装
-#@api.route('/get', methods=['GET'])
-#def get():
-#    return make_response(jsonify(all_list))
+@route('/get', methods=['GET'])
+def get():
+    return make_response(jsonify(all_list))
 
-@api.route('/', methods=['GET'])
+@route('/', methods=['GET'])
 def hello():
-    return print(hello)
+    return "hello"
 
-#@api.route('/upload', methods=['POST'])
-#def upload():
-#    if request.files and 'image' in request.files:
-#        img = request.files['image'].read()
-#        img = Image.open(io.BytesIO(img))
-#        img = img.resize((256,144))
-#        img_array = np.array(img)
-#        
-#        X_test = []
-#        X_test.append(img_array)
-#        X_test = np.asarray(X_test) / 255.
-#        model = models.model_vgg(n_class, HEIGHT, WIDTH)
-#        model.load_weights("all_weights12_25.h5")
-#        Yp = model.predict(X_test)
-#        predict = np.argmax(Yp)
-#        name = all_list[predict]
-#        data = dict(predict=str(predict), name=str(name))
-#        
-#        #return make_response(jsonify(all_list))
-#        return jsonify(data)
-#
-#    return 'Picture info did not get.'
-#
-## エラーハンドリング
-#@api.errorhandler(404)
+@route('/upload', methods=['POST'])
+def upload():
+    if request.files and 'image' in request.files:
+        img = request.files['image'].read()
+        img = Image.open(io.BytesIO(img))
+        img = img.resize((256,144))
+        img_array = np.array(img)
+        
+        X_test = []
+        X_test.append(img_array)
+        X_test = np.asarray(X_test) / 255.
+        model = models.model_vgg(n_class, HEIGHT, WIDTH)
+        model.load_weights("all_weights12_25.h5")
+        Yp = model.predict(X_test)
+        predict = np.argmax(Yp)
+        name = all_list[predict]
+        data = dict(predict=str(predict), name=str(name))
+        
+        #return make_response(jsonify(all_list))
+        return jsonify(data)
+
+    return 'Picture info did not get.'
+
+# エラーハンドリング
+#@errorhandler(404)
 #def not_found(error):
 #    return make_response(jsonify({'error': 'Not found'}), 404)
 
+run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 # ファイルをスクリプトとして実行した際に
 # ホスト0.0.0.0, ポート3001番でサーバーを起動
-if __name__ == '__main__':
-    api.debug = True
-    api.run()
+#if __name__ == '__main__':
+#    api.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
