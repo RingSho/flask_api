cba4f29ed8853d34a4b84f940f3c7908a2182fd1
林祥平 <s.0v0xx@outlook.jp>
2019-05-23 14:14:18 +0900

    changed

--- flask_api_test.py
+++ flask_api_test.py
@@ -41,6 +41,23 @@ all_list = fish_list + ebikani_list + ika_list + kai_list
 # __name__は現在のファイルのモジュール名
 api = Flask(__name__)
 
+import tensorflow as tf
+from keras import backend as K
+
+num_cores = 4
+GPU = False
+CPU = True
+
+if CPU:
+    num_CPU = 4
+    num_GPU = 0
+
+config = tf.ConfigProto(intra_op_parallelism_threads=num_cores,\
+        inter_op_parallelism_threads=num_cores, allow_soft_placement=True,\
+        device_count = {'CPU' : num_CPU, 'GPU' : num_GPU})
+session = tf.Session(config=config)
+K.set_session(session)
+
 
 # GETの実装
 @api.route('/get', methods=['GET'])
